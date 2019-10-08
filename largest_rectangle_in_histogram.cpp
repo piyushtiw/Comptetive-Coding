4fe1ec888dd85b28c16de8c790ff36cb8d15d668
piyushtiw <piyush@bigbinary.com>
2019-10-08 16:37:07 -0400

    largest rectangular area in histogram

---
+++ largest_rectangle_in_histogram.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+int largestRectangleArea(vector<int> &heights)
+{
+  vector<int> v = heights;
+  int a = 0;
+  int ma = 0;
+  int t;
+  int i = 0;
+  stack<int> s;
+
+  while (i < v.size())
+  {
+    if (s.empty() || v[i] >= v[s.top()])
+    {
+      s.push(i++);
+    }
+    else
+    {
+      t = s.top();
+      s.pop();
+      a = v[t] * (s.empty() ? i : i - s.top() - 1);
+      cout << t << " " << a << endl;
+      if (ma < a)
+        ma = a;
+    }
+  }
+
+  while (!s.empty())
+  {
+    t = s.top();
+    s.pop();
+    a = v[t] * (s.empty() ? i : i - s.top() - 1);
+    cout << "----------- " << t << " " << a << endl;
+    if (ma < a)
+      ma = a;
+  }
+
+  return ma;
+}
+
+
+int main(){
+  vector<int> v;
+  v.push_back(2);
+  v.push_back(0);
+  v.push_back(2);
+  v.push_back(1);
+  v.push_back(1);
+
+  cout << largestRectangleArea(v) << endl;
+  return 0;
+}
